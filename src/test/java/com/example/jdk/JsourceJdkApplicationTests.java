acbe41d4291e2af489598768422bb6db393c8c44
hujie <2580532608@qq.com>
2019-07-05 14:34:47 +0800

    Initial commit

---
+++ src/test/java/com/example/jdk/JsourceJdkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsourceJdkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
